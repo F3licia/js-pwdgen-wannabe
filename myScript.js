4aff1d78742da629f0f93448fa8a44afac97fa36
F3licia <romeofelicia@gmail.com>
2021-04-08 11:12:56 +0200

    correzione

--- myScript.js
+++ myScript.js
@@ -3,3 +3,5 @@ var textName = prompt("Inserisci il tuo nome");
 var textSurname = prompt("Inserisci il tuo cognome");
 var textColor = prompt("Qual è il tuo colore preferito?");
 
+document.getElementById("user").innerHTML= textName + " " + textSurname;
+document.getElementById("info").innerHTML= textColor;
